69e95c30a10d88a91a7600971c1ccd88948f7ade
Bobomurod04 <bobomurodj@outlook.com>
2022-09-09 17:56:01 +0200

    FAQ Section add

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>Any Questions <span>FAQs</span></h2>
+      <div className="question">
+        <h4>Kann ich auf dieser Plattform bei 0 anfangen zu programmieren?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing.       
+          </p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus!</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Kann ich auf dieser Plattform bei 0 anfangen zu programmieren?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing.       
+          </p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus!</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Kann ich auf dieser Plattform bei 0 anfangen zu programmieren?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing.       
+          </p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus!</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Kann ich auf dieser Plattform bei 0 anfangen zu programmieren?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing.       
+          </p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus!</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FaqSection;
--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutSection from '../AboutSection';
 import BenefitsSections from '../BenefitsSections';
+import FaqSection from '../FaqSection';
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <BenefitsSections />
+      <FaqSection />
     </>
   )
 }
